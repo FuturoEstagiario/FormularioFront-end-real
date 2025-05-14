3e0438d2131d993a68b8e7b5a6ab8ee52983f015
FuturoEstagiario <envieparabruno.rodrigues@gmail.com>
2025-05-13 22:02:03 -0300

    commit super errado da aula, onde foram integrados mais algumas coisas de JS

--- index.js
+++ index.js
@@ -1,5 +1,69 @@
-function enviar(){
-let alerta = 'Olá';
-alerta = document.getElementById("nome").value;
-alert("mensagem de aviso: " + alerta)
+class Contato{
+    //Atributos;
+    constructor() {
+        this.nome = null;
+        this.email = null;
+        this.fone = null;
+        this.mensagem = null;
+        this.data_cadastro = new date(); /* pega data atual do sistema operacional */
+        this.tipo_cadastro = null;
+    }
+
+    //Metodos;
+    Enviar(){
+        //if (nome esta vazio)mandar uma mensagem de erro;
+        // o '===' valida o valor e tipo da variavel;
+            
+        this.nome = document.getElementById("nome").value;
+        if (this.nome === ''){
+        alert += ', nome';
+        }
+        this.email = document.getElementById("email").value;
+        if (this.email === ''){
+        alert += ', email';
+        }
+        this.fone = document.getElementById("fone").value;
+        if (this.fone === ''){
+        alert += ', fone';
+        }
+        this.mensagem = document.getElementById("mensagem").value;
+        if (this.mensagem === ''){
+        alert += ', mensagem';
+        }
+        //this.data_cadastro = new date()
+        this.tipo_contato = document.getElementById("tipo_contato").value;
+        if (this.tipo_contato === '')
+        alert += ', tipo_contato';
+        }
+
+        if ('alert !=') 
+            alert('Os seguintes campos estõ com problema' + alerta);
+    }
+
 }
+
+let tale = document.getElementById('dados');
+let linha = tabela.insertRow();
+linha.insertCell();
+linha.innerHtml = this.nome;
+let linha = tabela.insertRow();
+linha.insertCell();
+linha.innerHtml = this.email;
+let linha = tabela.insertRow();
+linha.insertCell();
+linha.innerHtml = this.fone;
+let linha = tabela.insertRow();
+linha.insertCell();
+linha.innerHtml = this.mensagem;
+let linha = tabela.insertRow();
+linha.insertCell();
+linha.innerHtml = this.tipo_cadastro;
+
+// Instanciar a classe
+contato = new Contato();
+
+/*function submeter(){
+    var alerta = document.getElementById("nome").value;
+    alert("mensagem de aviso: " + alerta)
+}
+*/    
